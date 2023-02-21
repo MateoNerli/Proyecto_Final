c76595a28b1d188921bb811cd6ac8093f0a35149
Mateo <mnerli2003@gmail.com>
2023-02-21 14:40:46 -0300

    agrgamos banner

--- Proyecto_Final/src/app/app.module.ts
+++ Proyecto_Final/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoComponent } from './componentes/logo/logo.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
